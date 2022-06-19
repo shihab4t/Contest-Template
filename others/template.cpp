a3611c9921a0298731b1ccb189f76f9f366b099f
Shihab Mahamud <shihab4t@gmail.com>
2022-06-20 05:40:36 +0600

    all

---
+++ others/template.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using lli = long long int;
+using str = string;
+
+#define vec vector
+#define endn '\n'
+#define first_io                        \
+    ios_base::sync_with_stdio(false);   \
+    cin.tie(NULL);
+#define file_io                         \
+    freopen("input.txt", "r", stdin);   \
+    freopen("output.txt", "w", stdout);
+#define test_cases                      \
+    int t; cin >> t;while(t--) solve();
+
+template <typename tp>
+void print(tp arr[], int n) {
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << '\n';}
+template <typename tp>
+void print(vector<tp> &vc) {
+    for (auto &ith : vc)
+        cout << ith << " ";
+    cout << '\n';}
+
+
+void solve() {
+    
+}
+
+int main() {
+    first_io;
+
+    
+
+    return 0;
+}
+// Solved by: Shihab Mahamud (github.com/shihab4t)
