989df184f2e9a9012c74fe171e62f93c23482759
Shihab Mahamud <shihab4t@gmail.com>
2022-06-21 13:13:02 +0600

    Updates

--- template.cpp
+++ template.cpp
@@ -1,15 +1,75 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+template <typename T>
+istream &operator>>(istream &istream, vector<T> &v)
+{
+    int len = v.size();
+    for (int i = 0; i < len; i++)
+        cin >> v[i];
+    return istream;
+}
+template <typename T>
+ostream &operator<<(ostream &ostream, const set<T> &s)
+{
+    for (auto ith : s)
+        cout << ith << " ";
+    return ostream;
+}
+template <typename T>
+ostream &operator<<(ostream &ostream, const vector<T> &v)
+{
+    for (auto ith : v)
+        cout << ith << " ";
+    return ostream;
+}
+template <typename T, typename T2>
+ostream &operator<<(ostream &ostream, const pair<T, T2> &p)
+{
+    return (ostream << p.first << " " << p.second);
+}
+template <typename T, typename T2>
+ostream &operator<<(ostream &ostream, const map<T, T2> &mp)
+{
+    for (auto ith : mp)
+        cout << ith << "\n";
+    return ostream;
+}
+
 #define ed '\n'
 #define gp ' '
-#define i64 long long int
+#define lli long long int
 #define str string
 #define vec vector
 #define all(x) x.begin(), x.end()
 
 bool solve()
 {
+    lli n;
+    cin >> n;
+    vec<lli> v(n);
+    cin >> v;
+
+    lli prefix_val = v[0], suffix_removed = 0, ans = 0;
+
+    for (lli i = 1; i < n; i++)
+    {
+        v[i] -= suffix_removed;
+
+        if (v[i] <= prefix_val)
+        {
+            ans += prefix_val - v[i];
+            prefix_val = v[i];
+        }
+        else
+        {
+            suffix_removed += v[i] - prefix_val;
+            ans += v[i] - prefix_val;
+        }
+    }
+
+    ans += abs(prefix_val);
+    cout << ans << ed;
 
     return true;
 }
@@ -30,7 +90,7 @@ int32_t main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    // test();
+    test();
 
     return 0;
 }
