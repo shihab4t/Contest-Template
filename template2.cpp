ce41c2db72b19423a611b05a04a1082fd6503acb
Shihab Mahamud <shihab4t@gmail.com>
2022-07-05 08:15:15 +0600

    Update

--- template2.cpp
+++ template2.cpp
@@ -3,14 +3,31 @@ using namespace std;
 
 #define ed '\n'
 #define gp ' '
+#define lli long long int
+#define vec vector
+#define str string
+
+bool solve()
+{
+
+    return true;
+}
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    
+    // int t;
+    // cin >> t;
+    // while (t--)
+    // {
+    //     solve();
+    // }
+
+    // solve();
 
     return 0;
 }
 // github.com/sh048b (Shihab Mahamud)
+// 
